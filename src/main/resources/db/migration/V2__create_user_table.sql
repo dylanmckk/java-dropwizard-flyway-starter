80b8c18698625655daa0a615ae2c51a09229c1f6
dylanmckk <dylan.mckenzie@kainos.com>
2024-10-11 09:26:39 +0100

    Adding logging to database list

--- src/main/resources/db/migration/V2__create_user_table.sql
+++ src/main/resources/db/migration/V2__create_user_table.sql
@@ -1,7 +1,8 @@
-CREATE TABLE user (
-    id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    username varchar(50) NOT NULL,
-    password varchar(50) NOT NULL,
+CREATE TABLE user
+(
+    id        int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    username  varchar(50) NOT NULL,
+    password  varchar(50) NOT NULL,
     `Role id` int,
-    FOREIGN KEY (`Role id`) REFERENCES role(id)
+    FOREIGN KEY (`Role id`) REFERENCES role (id)
 );
